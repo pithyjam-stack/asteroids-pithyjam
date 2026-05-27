934bbf89acff5568b55a1b3e2b18acd45c3abb05
pithyjam-stack <pithyjam@gmail.com>
2026-05-27 13:28:47 -0700

    C: reduce fps

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
+    clock = pygame.time.Clock()
+    dt: float = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
